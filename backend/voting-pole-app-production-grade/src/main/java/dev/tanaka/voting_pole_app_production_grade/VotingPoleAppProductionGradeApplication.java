cc0ded1e19c7441abd74efa25b221fbe54b8b134
TanakaZuze <99540354+TanakaZuze@users.noreply.github.com>
2025-09-02 15:50:28 +0200

    Initial commit

---
+++ backend/voting-pole-app-production-grade/src/main/java/dev/tanaka/voting_pole_app_production_grade/VotingPoleAppProductionGradeApplication.java
@@ -0,0 +1,13 @@
+package dev.tanaka.voting_pole_app_production_grade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingPoleAppProductionGradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingPoleAppProductionGradeApplication.class, args);
+	}
+
+}
---
+++ backend/voting-pole-app-production-grade/src/test/java/dev/tanaka/voting_pole_app_production_grade/VotingPoleAppProductionGradeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tanaka.voting_pole_app_production_grade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingPoleAppProductionGradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
